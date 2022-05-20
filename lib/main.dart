9c3fcd1541945c3773d0d8d4119cb9d6b8050926
dabuzida <dabuzida@gmail.com>
2022-05-20 18:12:40 +0900

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +9,281 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'ThreeTaps',
+      home: AAA(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AAA extends StatelessWidget {
+  const AAA({Key? key}) : super(key: key);
+  final double _infinity = double.infinity;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ThreeTaps'),
+        elevation: 0,
+        foregroundColor: Colors.cyanAccent,
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: ThreeTaps(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ThreeTaps extends StatefulWidget {
+  const ThreeTaps({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
+  @override
+  State<ThreeTaps> createState() => _ThreeTapsState();
+}
+
+class _ThreeTapsState extends State<ThreeTaps> {
+  // List items = ['field1', 'field2', 'field3'];
+  int _selectedIndex = 0;
+  final Color _activeColor = const Color.fromARGB(255, 243, 245, 247);
+  final Color _inactiveColor = const Color.fromARGB(255, 255, 255, 255);
+  final Color _borderColor = const Color.fromARGB(255, 220, 226, 228);
+  bool _visibleA = true;
+  bool _visibleB = false;
+  bool _visibleC = false;
+  void _onTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (index == 0) {
+        _visibleA = true;
+        _visibleB = false;
+        _visibleC = false;
+      } else if (index == 1) {
+        _visibleA = false;
+        _visibleB = true;
+        _visibleC = false;
+      } else {
+        _visibleA = false;
+        _visibleB = false;
+        _visibleC = true;
+      }
     });
   }
 
+  final List _tapPhrase = [
+    {'num': '01', 'title': '고객 추가'},
+    {'num': '02', 'title': '새 캠페인'},
+    {'num': '03', 'title': '알츠윈 콜'},
+  ];
+  final Color _tapPhraseColor = const Color.fromARGB(255, 0, 71, 255);
+  /* @override
+  void initState() {
+    super.initState();
+    _miniTapAColor = _activeColor;
+    _miniTapBColor = _inactiveColor;
+    _miniTapCColor = _inactiveColor;
+  } */
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      color: Colors.white,
+      // padding: EdgeInsets.symmetric(horizontal: 50),
+      // width: 1000,
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      height: MediaQuery.of(context).size.height,
+
+      // height: 500,
+      // constraints: const BoxConstraints(
+      //   maxWidth: 700,
+      //   minHeight: 500,
+      // ),
+      // constraints: BoxConstraints.expand(),
+      child: Stack(
+        // alignment: Alignment.bottomCenter,
+        // clipBehavior: Clip.antiAliasWithSaveLayer,
+        // fit: StackFit.passthrough,
+        children: <Widget>[
+          Positioned(
+            child: Row(
+              children: <Widget>[
+                _lowerTap(id: 0),
+                const SizedBox(width: 10),
+                _lowerTap(id: 1),
+                const SizedBox(width: 10),
+                _lowerTap(id: 2),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 60,
+            child: Container(
+              // width: 700,
+              width: MediaQuery.of(context).size.width - 200,
+              height: MediaQuery.of(context).size.height,
+              // margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                color: _activeColor,
+                border: Border.all(color: _borderColor, width: 1),
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(5.0),
+                  bottomLeft: Radius.circular(5.0),
+                  bottomRight: Radius.circular(5.0),
+                ),
+              ),
+              child: Center(
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(children: [
+                          Container(width: 50, height: 50, color: Colors.blue),
+                          Container(width: 50, height: 50, color: Colors.yellow),
+                          Container(width: 50, height: 50, color: Colors.red),
+                        ]),
+                        Container(width: 50, height: 50, color: Colors.teal),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 200,
+                          height: 200,
+                          decoration: const BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(100),
+                              topRight: Radius.circular(100),
+                              // bottomLeft: Radius.circular(40.0),
+                              bottomRight: Radius.circular(40.0),
+                            ),
+                          ),
+                        ),
+                        Container(color: Color.fromARGB(255, 178, 180, 17), width: 50, height: 50),
+                        // Container(color: Color.fromARGB(255, 14, 115, 216), width: 50, height: 50),
+                        // Container(color: Color.fromARGB(255, 189, 22, 22), width: 50, height: 50),
+                        Container(color: Color.fromARGB(255, 15, 124, 160), width: 50, height: 50),
+                      ],
+                    ),
+                    Container(
+                      width: 200,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        border: Border.all(color: Colors.blueAccent, width: 10),
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        border: Border.all(color: Colors.red, width: 5),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 1,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const SizedBox(width: 1),
+                _upperTap(id: 0, visible: _visibleA),
+                const SizedBox(width: 12),
+                _upperTap(id: 1, visible: _visibleB),
+                const SizedBox(width: 12),
+                _upperTap(id: 2, visible: _visibleC),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  Widget _lowerTap({required int id}) {
+    return Container(
+      width: 130,
+      height: 61,
+      decoration: BoxDecoration(
+        color: _inactiveColor,
+        border: Border.all(color: _borderColor, width: 1),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(5), // bottomLeft: Radius.circular(40.0),
+          topRight: Radius.circular(5), // bottomRight: Radius.circular(40.0),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Center(
+        child: Row(children: <Widget>[
+          const SizedBox(
+            width: 4,
+            height: 40,
+          ),
+          const SizedBox(width: 8),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_tapPhrase[id]['num']),
+              Text(_tapPhrase[id]['title']),
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+
+  Widget _upperTap({required int id, required bool visible}) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+          onTap: () => _onTapped(id),
+          child: Visibility(
+            visible: visible,
+            maintainSize: true,
+            maintainAnimation: true,
+            maintainState: true,
+            maintainInteractivity: true,
+            child: Container(
+              width: 128,
+              height: 60,
+              decoration: BoxDecoration(
+                color: _activeColor,
+                // color: Colors.greenAccent,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(5), // bottomLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(5), // bottomRight: Radius.circular(40.0),
+                ),
+              ),
+              child: Center(
+                child: Row(children: <Widget>[
+                  Container(
+                    width: 4,
+                    height: 40,
+                    color: _tapPhraseColor,
+                  ),
+                  const SizedBox(width: 8),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(_tapPhrase[id]['num'], style: TextStyle(color: _tapPhraseColor)),
+                      Text(_tapPhrase[id]['title'], style: TextStyle(color: _tapPhraseColor)),
+                    ],
+                  )
+                ]),
+              ),
+            ),
+          )),
     );
   }
 }
